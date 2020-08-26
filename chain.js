5173ce4eacda1fa5091ed82f16cf16837dffe07b
Jiya26 <68414702+Jiya26@users.noreply.github.com>
2020-08-26 22:33:44 +0530

    Add files via upload

--- chain.js
+++ chain.js
@@ -1,10 +1,12 @@
 class Chain{
-    constructor(bodyA,bodyB){
+    constructor(bodyA,bodyB,offsetX,offsetY){
+        this.offsetX=offsetX
+        this.offsetY=offsetY
         var options = {
             bodyA:bodyA,
             bodyB:bodyB,
-            length:200,
-            stiffness:0.5
+            stiffness:0.5,
+            pointB:{x:this.offsetX,y:this.offsetY}
         }
         this.chain=Constraint.create(options)
         World.add (world,this.chain)
@@ -14,6 +16,12 @@ class Chain{
         strokeWeight(5)
         var pointA = this.chain.bodyA.position
         var pointB = this.chain.bodyB.position
+
+        var Anchor1X = pointA.x
+        var Anchor1Y = pointA.y
+
+        var Anchor2X = pointB.x+this.offsetX
+        var Anchor2Y = pointB.y+this.offsetY
         line(pointA.x,pointA.y,pointB.x,pointB.y)
         pop()
     }
